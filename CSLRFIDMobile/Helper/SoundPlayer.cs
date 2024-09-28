a9708586968246fdf63bcff9817de0065dc1f433
Carlson Lam <4546682+ksclam@users.noreply.github.com>
2024-09-28 01:24:15 -0400

    Bug fix on Geiger audio

--- CSLRFIDMobile/Helper/SoundPlayer.cs
+++ CSLRFIDMobile/Helper/SoundPlayer.cs
@@ -1,4 +1,5 @@
-﻿using Plugin.Maui.Audio;
+﻿using Microsoft.Maui.Storage;
+using Plugin.Maui.Audio;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,16 +10,9 @@ namespace CSLRFIDMobile.Helper
 {
     public static class SoundPlayer
     {
-        public static void PlaySound(IAudioManager manager, string wavFile)
+        public static async Task PlaySound(IAudioManager manager, string wavFile)
         {
-            var wavFolder = Path.Combine(FileSystem.Current.AppDataDirectory, "Resources", "Raw");
-            if (!Directory.Exists(wavFolder))
-            {
-                return;
-            }
-            string wavFileFullPath = Path.Combine(wavFolder, wavFile);
-
-            Stream wavStream = File.OpenRead(wavFileFullPath);
+            Stream wavStream = await FileSystem.Current.OpenAppPackageFileAsync(wavFile);
 
             var player = manager.CreatePlayer(wavStream);
             player.Play();
